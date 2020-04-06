c71490e3da4f00328b718122eb98376c1ed791f4
Gene Leynes <Gene.Leynes@cityofchicago.org>
2020-04-06 00:29:14 -0500

    separated out the today definition

--- WardReport_v2/20_render_reports.R
+++ WardReport_v2/20_render_reports.R
@@ -1,17 +1,20 @@
 # geneorama::clipdir()
 
 # i=2
+
+## Define "today" to be static
+today <- Sys.Date()
+
 for(i in 1:50){
   print(i)
   yaml::write_yaml(list("cur_ward" = i), "WardReport_v2/cur_ward.yaml")
   fname <- sprintf("WardReport_v2/output/report_%s/Ward_%02i_%s.html", 
-                   Sys.Date(),
-                   as.integer(i),
-                   Sys.Date())
+                   today, as.integer(i), today)
   if(!file.exists(dirname(fname))) dir.create(dirname(fname))
   rmarkdown::render(input = "WardReport_v2/WardReport.Rmd",
                     output_dir = dirname(fname),
-                    output_file = basename(fname))
+                    output_file = basename(fname),
+                    quiet = TRUE)
 }
 
 
